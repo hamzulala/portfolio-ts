49b8e42b9bade744fcadc58b5b08988e1abba077
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-01-16 23:26:11 +1100

    Created Hero Section
    - Hero Component Made
    - BackgroundCircles Component Made (this component has the background effect for the circles)
    - Typewriter Text Effect Set
    - Added Static Profile Picture
    - Added navigation buttons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,44 @@
+import Link from 'next/link';
 import React from 'react'
+import { Cursor, useTypewriter } from 'react-simple-typewriter'
+import BackgroundCircles from './BackgroundCircles';
 
 type Props = {}
 
 function Hero({}: Props) {
+    const [text, count] = useTypewriter({
+        words: [
+            "Hi, The Name's Hamza Ahmed",
+            "Full-Stack-Developer.tsx",
+            "Web Developer",
+            "TypeScript Developer",
+            "JavaScript Developer",
+            "React Native Developer",
+            "ReactJS Developer",
+            "Android Developer",
+        ],
+        loop: true,
+        delaySpeed: 2000,
+    });
+
   return (
-    <div>Hero</div>
+    <div className='h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden'>
+        <BackgroundCircles/>
+        <img className='relative rounded-full h-32 w-32 mx-auto object-cover' src="/profile-pic.png" alt="Hamza's Profile Picture"/>
+        <div className='z-20'>
+            <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[10px]'>FullStack Developer</h2>
+            <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
+                <span className='mr-3'>{text}</span>
+                <Cursor cursorColor='#F7AB0A'/>
+            </h1>
+        </div>
+        <div className='pt-5'>
+            <Link href="#about" ><button className='heroButton'>About</button></Link>
+            <Link href="#experience" ><button className='heroButton'>Experience</button></Link>
+            <Link href="#skills" ><button className='heroButton'>Skills</button></Link>
+            <Link href="#projects" ><button className='heroButton'>Projects</button></Link>
+        </div>
+    </div>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import Hero from '@/components/Hero';
 
 const Home: NextPage = () => {
   return (
-    <>
+    <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Hamza's Portfolio</title>
       </Head>
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
       <Header/>
 
       {/* Hero */}
-      <section id="hero">
+      <section id="hero" className='snap-center'>
         <Hero/>
       </section>
 
@@ -28,7 +28,7 @@ const Home: NextPage = () => {
 
       {/* Contact Me */}
 
-    </>
+    </div>
   );
 };
 

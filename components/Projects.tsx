91bbfe734f6727907e29d951b55b7ee6fd09f4f4
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-01-17 18:36:35 +1100

    Created Projects and Skills Section
    - Skills Component Created with styling
    - Projects Component Created with styling

---
+++ components/Projects.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { motion } from 'framer-motion';
+
+type Props = {}
+
+function Projects({}: Props) {
+    const projects = [1,2,3,4,5];
+  return (
+    <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:1.5}} className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
+        <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl z-10'>Projects</h3>
+        <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20'>
+            {/*Projects */}
+            {projects.map((project) => (
+                <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+                    <motion.img initial={{y:-300, opacity:0}} whileInView={{opacity:1, y:0}} transition={{duration:1.2}} viewport={{once:true}} src="https://freepngimg.com/thumb/responsive_web_design/4-2-responsive-web-design-picture.png" alt="responsive web design icon"/>
+                    <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
+                        <h4 className='text-4xl font-semibold text-center'><span className='underline decoration-[#F7ABOA]/50'>Clocs-A:</span> A journey location Identifier</h4>
+                        <p className='text-lg text-center md:text-left'>CLOCS-A is a national initiative to implement safety standards for the construction truck industry across Australia. For the last year, we've been working with them to create a system of three applications to help the people involved in construction logistics with safety checks, communication and documentation.</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+        <div className='w-full absolute top-[30%] bg-[#F7AB0A]/10 left-0 h-[500px] -skew-y-12'/>
+    </motion.div>
+  )
+}
+
+export default Projects
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,8 @@ import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
 import WorkExperience from '@/components/WorkExperience';
+import Skills from '@/components/Skills';
+import Projects from '@/components/Projects';
 
 
 const Home: NextPage = () => {
@@ -31,8 +33,14 @@ const Home: NextPage = () => {
       </section>
 
       {/* Skills */}
+      <section id="skills" className='snap-start'>
+        <Skills/>
+      </section>
 
       {/* Projects */}
+      <section id="projects" className='snap-start'>
+        <Projects/>
+      </section>
 
       {/* Contact Me */}
 

8b891a992ec5ad5523f36764fc9247116bcf1115
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-01-23 14:53:58 +1100

    Fixed Build Errors

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -14,7 +14,7 @@ function Projects({ projects }: Props) {
         <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory p-10 md:p-30 z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
             {/*Projects */}
             {projects.map((project, i) => (
-                <div className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
+                <div key={project._id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
                     <motion.img initial={{y:-300, opacity:0}} whileInView={{opacity:1, y:0}} transition={{duration:1.2}} viewport={{once:true}} src={urlFor(project?.image).url()} alt="responsive web design icon"/>
                     <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
                         <h4 className='text-4xl font-semibold text-center'>{project.title}</h4>

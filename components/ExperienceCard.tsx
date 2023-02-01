a9faf50c9e1abf78910b19e98c3f3d44e66d93ee
Hamza Ahmed <Hamzaunaiz@gmail.com>
2023-02-01 22:42:28 +1100

    Fixed Mobile View Bugs
    - Fixed Experience Card Height and width for small screens
    - Fixed Projects tab height and width for small screens

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -9,9 +9,9 @@ type Props = {
 
 function ExperienceCard({experience}: Props) {
   return (
-    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity overflow-hidden'>
+    <article className='flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-screen h-2/3 md:h-[600px] mt-20 md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity overflow-hidden'>
         <motion.img initial={{y:-100, opacity: 0,}} transition={{duration:1.2}} whileInView={{opacity:1, y:0}} viewport={{once:true}} className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-center object-scale-down' src={urlFor(experience?.companyImage).url()} alt="CLOCS-A Icon" />
-        <div className='px-0 md:px-10'>
+        <div className='px-10 md:px-10 overflow-y-scroll scrollbar-thin scrollbar-thumb-[#F7AB0A]/80'>
             <h4 className='text-4xl font-light'>{experience.jobTitle}</h4>
             <p className='font-bold text-2xl mt-1'>{experience.company}</p>
             <div className='flex space-x-2 my-2'>
@@ -24,7 +24,7 @@ function ExperienceCard({experience}: Props) {
               {new Date(experience.dateStarted).toDateString()} -{" "}
               {experience.isCurrentlyWorkingHere ? "Present" : new Date(experience.dateEnded).toDateString()}
             </p>
-            <ul className='list-disc space-y-4 ml-5 text-lg max-h-96 overflow-y-scroll pr-5 scrollbar-thin scrollbar-track-black scrollbar-thumb-[#F7AB0A]/80'>
+            <ul className='list-disc space-y-4 ml-5 text-lg max-h-96 overflow-y-scroll pr-5 scrollbar-thin'>
               {experience.points.map((point, i) => (
                 <li key={i}>{point}</li>
               ))}
--- components/Projects.tsx
+++ components/Projects.tsx
@@ -11,20 +11,23 @@ function Projects({ projects }: Props) {
   return (
     <motion.div initial={{opacity:0}} whileInView={{opacity:1}} transition={{duration:1.5}} className='h-screen relative flex overflow-hidden flex-col text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl z-10'>Projects</h3>
-        <div className='relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory p-10 md:p-30 z-20 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
+        <div className='relative w-full h-2/3 flex overflow-x-scroll overflow-y-scroll snap-x snap-mandatory p-10 md:p-30 z-20 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80'>
             {/*Projects */}
             {projects.map((project, i) => (
                 <div key={project._id} className='w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen'>
-                    <motion.img initial={{y:-300, opacity:0}} whileInView={{opacity:1, y:0}} transition={{duration:1.2}} viewport={{once:true}} src={urlFor(project?.image).url()} alt="responsive web design icon"/>
-                    <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
-                        <h4 className='text-4xl font-semibold text-center'>{project.title}</h4>
-                        <div className='flex items-center space-x-2 justify-center'>
-                            {project?.technologies.map(technology=> (
-                                <img className='h-10 w-10' key={technology._id} src={urlFor(technology.image).url()} alt="" />
-                            ))}
+                    <div className=''>
+                        <motion.img initial={{y:-300, opacity:0}} whileInView={{opacity:1, y:0}} transition={{duration:1.2}} viewport={{once:true}} src={urlFor(project?.image).url()} alt="responsive web design icon"/>
+                        <div className='space-y-10 px-0 md:px-10 max-w-6xl'>
+                            <h4 className='text-4xl font-semibold text-center'>{project.title}</h4>
+                            <div className='flex items-center space-x-2 justify-center'>
+                                {project?.technologies.map(technology=> (
+                                    <img className='h-10 w-10' key={technology._id} src={urlFor(technology.image).url()} alt="" />
+                                ))}
+                            </div>
+                            <p className='text-lg text-center md:text-left'>{project?.summary}</p>
                         </div>
-                        <p className='text-lg text-center md:text-left'>{project?.summary}</p>
                     </div>
+                    
                 </div>
             ))}
         </div>
